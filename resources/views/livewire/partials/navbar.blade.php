643b8c83ae1d1dba9cb61018056af83700c23590
rohmath8 <rohmathidayattullah97@gmail.com>
2024-12-16 08:36:13 +0700

    style

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -14,7 +14,7 @@
             </div>
         @endif
     </div>
-    <div class="flex gap-3">
+    <div class="flex gap-3 items-center justify-center">
         {{-- @if (auth()->user()->role === 'admin') --}}
             <svg id="filterIcon" class="w-6 h-6 text-gray-800 hover:text-gray-500 cursor-pointer"
                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
--- resources/views/livewire/partials/navbaruser.blade.php
+++ resources/views/livewire/partials/navbaruser.blade.php
@@ -2,7 +2,7 @@
     <div class="flex gap-3 items-center">
         <a href="/"><img src="{{asset('assets/logo.png')}}" class="h-6 mx-auto"  alt="logo"/></a>
     </div>
-    <div class="flex gap-3">
+    <div class="flex gap-3 items-center justify-center">
         <!-- Ikon User -->
         <svg id="userIcon" class="w-6 h-6 text-gray-800 hover:text-gray-500 cursor-pointer"
             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
